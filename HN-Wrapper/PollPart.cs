7a5f51d29da46028548af5b153130798b8c6bd15
Matthew O'Neill <14matthewoneill14@gmail.com>
2014-10-08 04:06:48 +0100

    removed unwanted usings

--- HN-Wrapper/PollPart.cs
+++ HN-Wrapper/PollPart.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HN_Wrapper
+﻿namespace HN_Wrapper
 {
     public class PollPart : Item
     {
